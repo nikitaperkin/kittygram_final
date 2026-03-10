e3422849a0c6a46aa3f8e74cd5e7ba0cd010f871
Nikita Perkin <perkin1999@gmail.com>
2026-03-10 15:08:51 +0300

    Update Kittygram_final

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -4,11 +4,11 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '158.160.160.70', 'alpina.hopto.org']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '158.160.160.70', 'goodkittylife.hopto.org']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
